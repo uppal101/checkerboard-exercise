63f92365a03b1c0ece96b823777c6b57c9a26c1e
Sanjeet Uppal <sanjeet.uppal92@gmail.com>
2017-01-19 18:04:30 -0800

    checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // Your JS goes here
+
+
+
+for(let i = 0; i < 81; i++) {
+  var tile = document.createElement('div');
+  tile.style.paddingBottom = "11.1%";
+  tile.style.width = "11.1%";
+  tile.style.backgroundColor = color;
+  tile.style.float = "left";
+  var r = Math.floor(Math.random() * 255);
+  var g = Math.floor(Math.random() * 255);
+  var b = Math.floor(Math.random() * 255);
+  var col = "rgb(" + r + "," + g + "," + b + ")";
+
+  // if(i % 2 === 0) {
+  //   tile.style.backgroundColor = "black";
+  // }
+
+  document.body.appendChild(tile);
+}
